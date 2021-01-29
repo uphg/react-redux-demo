17403cb912deb546c93ac71b3a54ea774213ac65
lvheng <1834805770@qq.com>
2021-01-29 21:52:30 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p className="message">
+          {props.message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button
+            className="margin-right-10"
+            onClick={props.onAdd}
+          >点击加一</button>
+          <button
+            onClick={props.onBack}
+          >点击减一</button>
+        </div>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,12 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { createStore } from 'redux'
+
+const stateChange = (state = 0, action) => {
+  if (action.type === 'add') {
+    return state + action.payload
+  } else if( action.type === 'back') {
+    return state - action.payload
+  } else {
+    return state
+  }
+}
+
+const store = createStore(stateChange)
+
+render()
+store.subscribe(render)
+
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        message={store.getState()}
+        onAdd={() => { store.dispatch({ type: 'add', payload: 1 }) }}
+        onBack={() => { store.dispatch({ type: 'back', payload: 1 }) }}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
