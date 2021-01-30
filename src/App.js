d8e26d36febab2609347fbf509dcea70fdaea6e0
lvheng <1834805770@qq.com>
2021-01-30 16:07:31 +0800

    添加组件事件多种调用函数的方式

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App(props) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p className="message">
-          {props.message}
-        </p>
-        <div>
-          <button
-            className="margin-right-10"
-            onClick={props.onAdd}
-          >点击加一</button>
-          <button
-            onClick={props.onBack}
-          >点击减一</button>
-        </div>
-      </header>
-    </div>
-  );
+class App extends Component {
+  add() {
+    this.props.onAdd()
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p className="message">
+            {this.props.message}
+          </p>
+          <div>
+            <button
+              className="margin-right-10"
+              onClick={() => { this.add() }}
+            >点击加一</button>
+            <button
+              onClick={this.props.onBack}
+            >点击减一</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
